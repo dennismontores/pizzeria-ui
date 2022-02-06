9330aeab9bb5b4a94a149f1026318e740e8f8384
Dennis Montores <dennis.montores@outlook.com>
2022-02-06 13:21:45 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import { useState, useEffect } from 'react'
 import CSS from './App.module.css'
 import { Routing } from './navigation'
 
 const App = () => {
-  // Create the count state.
-  const [count, setCount] = useState(0)
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000)
-    return () => clearTimeout(timer)
-  }, [count, setCount])
-  // Return the App component.
   return (
     <div className={CSS.App}>
       <h1>Pizzeria</h1>
